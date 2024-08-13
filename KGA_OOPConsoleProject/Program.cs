23b61e31a4dae5045f05fa9e7b09f0d54b82eca9
Geniards <geniard93@gmail.com>
2024-08-13 12:13:05 +0900

    [Test]: GamePlay
    test Player & Maze GamePlay

--- KGA_OOPConsoleProject/Program.cs
+++ KGA_OOPConsoleProject/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace KGA_OOPConsoleProject
+﻿using KGA_OOPConsoleProject.Input;
+
+namespace KGA_OOPConsoleProject
 {
     public class Program
     {
@@ -8,11 +10,37 @@
             maze.Generate();
             maze.Render();
 
-            Player player = new Player("ABC");
+            Player player = new Player("ABC", maze.GetCount());
+            Monster obstacle = new Obstacle("ABC", 2, (3, 3));
+            InputComponent input = new InputComponent();
 
+            (int x, int y) pos = player.pos;
+            int count = player.count;
             
+            while (pos.x != maze.GetGraph().GetLength(0)-2 || pos.y != maze.GetGraph().GetLength(1) - 2)
+            {
+                Console.Clear();
+                maze.Render();
+                int currLeft = Console.CursorLeft;
+                int currTop = Console.CursorTop;
+                player.Generate();
+                Console.SetCursorPosition(currLeft, currTop);
+                player.Render();
+
+                input.Move(maze.GetGraph(), ref pos, ref count);
+                player.pos = pos;
+                player.count = count;
+
+                Console.WriteLine("====================");
+                Console.WriteLine("Player");
+                Console.WriteLine(player.pos);
+                Console.WriteLine(player.count);
+
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("도착");
         }
 
-        
+
     }
 }
