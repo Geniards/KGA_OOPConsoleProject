ae9c88bc01c31943ecf8cab6fda9b810abd8f4ec
Geniards <geniard93@gmail.com>
2024-08-16 03:18:58 +0900

    [Create]: ItemManager
    아이템 생성 클래스

---
+++ KGA_OOPConsoleProject/03.Item/Item/ItemManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGA_OOPConsoleProject._03.Item.Item
+{
+    public class ItemManager
+    {
+        private Items potion;
+        private Items mapResearch;
+        private Items jump;
+
+        private List<Items> items;
+
+        public bool bCreateItem;
+
+
+        public ItemManager()
+        {
+            bCreateItem = false;
+            items = new List<Items>();
+            Init();
+        }
+
+        private void Init()
+        {
+            potion = ItemFactory.Instantiate("Potion");
+            mapResearch = ItemFactory.Instantiate("MapReSearch");
+            jump = ItemFactory.Instantiate("Jump");
+
+            items.Add(potion);
+            items.Add(mapResearch);
+            items.Add(jump);
+        }
+
+        private Items RandomItem()
+        {
+            Random rd = new Random();
+            int num = rd.Next(0, items.Count);
+
+            return items[num]; 
+        }
+
+        public void Create()
+        {
+            bCreateItem = true;
+        }
+
+        public Items GetItem()
+        {
+            if (bCreateItem)
+            {
+                bCreateItem = false;
+                return RandomItem();
+            }
+            else
+                return null;
+        }
+    }
+}
