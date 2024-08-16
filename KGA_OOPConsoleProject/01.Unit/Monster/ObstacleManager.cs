217896d380061e86566e162fdf7cf79fe36b9885
Geniards <geniard93@gmail.com>
2024-08-16 14:45:17 +0900

    [Feat] Collison 기능 추가
    ObstacleManager가 Collision기능 담당

--- KGA_OOPConsoleProject/01.Unit/Monster/ObstacleManager.cs
+++ KGA_OOPConsoleProject/01.Unit/Monster/ObstacleManager.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using KGA_OOPConsoleProject._03.Item.Item;
+using KGA_OOPConsoleProject.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +12,12 @@ namespace KGA_OOPConsoleProject._01.Unit.Monster
     public class ObstacleManager
     {
         private Maze maze = Maze.Instance;
+        private ItemManager itemManager;
+
+        public ObstacleManager(ItemManager itemManager)
+        {
+            this.itemManager = itemManager;
+        }
 
         public Obstacle[] CreateObstacles(int count)
         {
@@ -30,6 +39,39 @@ namespace KGA_OOPConsoleProject._01.Unit.Monster
             }
 
             return obstacles;
+        }
+
+        public Items Obstacle_Coll(ref int dir, ref Player player, ref Obstacle[] obstacles)
+        {
+            (int, int)[] dirPos = { (-1, 0), (1, 0), (0, -1), (0, 1) };
+
+            if (dir - 1 > dirPos.Length)
+                return null;
+
+            // 장애물 충돌 && 벽에 닿으면 박살
+            for (int i = 0; i < obstacles.Length; i++)
+            {
+                if (player.pos == obstacles[i].pos)
+                {
+                    player.hp--;
+                    int x = obstacles[i].pos.Item1;
+                    int y = obstacles[i].pos.Item2;
+
+                    if (maze.search(x + dirPos[dir - 1].Item1, y + dirPos[dir - 1].Item2))
+                    {
+                        obstacles[i].pos = (x + dirPos[dir - 1].Item1, y + dirPos[dir - 1].Item2);
+                        return null;
+                    }
+                    else
+                    {
+                        obstacles[i].state = EState.Dead;
+                        itemManager.Create();
+                        return itemManager.GetItem();
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
