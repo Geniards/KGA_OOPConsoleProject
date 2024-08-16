9adf2176db80b37658685377944c22e417221031
Geniards <geniard93@gmail.com>
2024-08-16 10:09:42 +0900

    [Create] EState
    EShortKey, EStage를 생성

--- KGA_OOPConsoleProject/00.Util/Util/EState.cs
+++ KGA_OOPConsoleProject/00.Util/Util/EState.cs
@@ -15,4 +15,14 @@ namespace KGA_OOPConsoleProject.Util
     {
         Potion, MapReSearch, Jump, EITEM_TYPE_MAX
     }
+
+    public enum EShortKey
+    {
+        None = 0, Up, Down, Left, Right, Inven = 6, ESHORTKEY_MAX
+    }
+
+    public enum EStage
+    {
+        None, ESTAGE_MAX = 3
+    }
 }
